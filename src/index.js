d0673864c75fc5b5dad93cb5d7946e57cf94ad10
niki <nikolayp1707@gmail.com>
2021-11-04 14:39:04 +0200

    add app component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <LoginForm />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
